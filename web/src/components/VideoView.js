a5ddf07a642d8abba3b98512ba54728d3ea7c50a
stepa <awelijuh@gmail.com>
2022-02-22 10:09:58 +0300

    fix

--- web/src/components/VideoView.js
+++ web/src/components/VideoView.js
@@ -7,7 +7,7 @@ export function VideoView({stream}) {
 
     return (
         <Container className="d-flex">
-            <img className="ms-auto me-auto" style={{height: 'calc(100vh - 70px)'}} src={stream}/>
+            <img className="ms-auto me-auto w-100" style={{maxHeight: 'calc(100vh - 70px)'}} src={stream}/>
         </Container>
     )
 }
